c5e09909a8eb3f241302be7990307e7f2c08f9bb
erobert <etiennerobert33@gmail.com>
2019-06-17 16:01:38 +0200

    add rudimentary math functions

--- math.h
+++ math.h
@@ -8,18 +8,9 @@ T pow(T x, unsigned int n) {
   return r;
 }
 
-unsigned int log(unsigned int base, unsigned int x) {
-  unsigned int l(0);
-  while ((x /= base) != 0)
-    ++l;
-  return l;
-}
+unsigned int log(unsigned int base, unsigned int x);
 
-unsigned int rough_sqrt(unsigned int x) {
-  const unsigned int n = log(100, x);
-  const unsigned int a = x / pow(100, n);
-  return (a < 10 ? 2 : 6) * pow(10, n);
-}
+unsigned int rough_sqrt(unsigned int x);
 
 template<typename T>
 double sqrt_r(T s, double x, unsigned int iter) {
@@ -28,7 +19,7 @@ double sqrt_r(T s, double x, unsigned int iter) {
   return sqrt_r(s, (x + s / x) / 2, iter - 1);
 }
 
-// x should be positive
+// s should be positive
 template<typename T>
 double sqrt(T s, unsigned int iter = 10) {
   return sqrt_r(s, static_cast<double>(rough_sqrt(s)), iter);
