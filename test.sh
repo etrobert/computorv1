096a56ea4580ae891a188dac18ce9a39bbd27c43
erobert <etiennerobert33@gmail.com>
2019-06-21 16:34:47 +0200

    update tests

--- test.sh
+++ test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-diff <(./computor "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0") tests/1.out
-diff <(./computor "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0") tests/2.out
-diff <(./computor "5 * X^0 + 4 * X^1 = 4 * X^0") tests/3.out
-diff <(./computor "1.5 * X^0 + 0 * X^1 + 1.5 * X^2 = 0") tests/4.out
-diff <(./computor "0 * X^0 = 0 * X^0") tests/5.out
-diff <(./computor "0 * X^0 = 1 * X^1") tests/6.out
+COLORGREEN='\033[92m'
+COLOREND='\033[0m'
+COLORRED='\033[31m'
+
+test () {
+  echo "Testing $1 against $2"
+  diff <(./computor "$1") $2\
+    && echo -e "$COLORGREEN TEST SUCCESS $COLOREND"\
+    || echo -e "$COLORRED TEST ERROR $COLOREND"
+}
+
+test "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" tests/1.out
+test "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0" tests/2.out
+test "5 * X^0 + 4 * X^1 = 4 * X^0" tests/3.out
+test "1.5 * X^0 + 0 * X^1 + 1.5 * X^2 = 0 * X^0" tests/4.out
+test "0 * X^0 = 0 * X^0" tests/5.out
+test "0 * X^0 = 1 * X^1" tests/6.out
+test "1 * X^0 = -1 * X^1" tests/7.out
+test "0 * X^0 + 0 * X^1 - 1 * X^2 = 0 * X^0" tests/8.out
