1d155b7675b648ec42731cd0b5a42abc0de683df
erobert <etiennerobert33@gmail.com>
2019-11-08 15:33:08 +0100

    add int regex

--- get_regex.h
+++ get_regex.h
@@ -4,6 +4,11 @@ template<typename T>
 std::string_view get_regex();
 
 template<>
+std::string_view get_regex<int>() {
+  return "[0-9]+";
+}
+
+template<>
 std::string_view get_regex<float>() {
   return "[0-9]+(\\.[0-9]+)?";
 }
